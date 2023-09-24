c98ca4cb18d052b644796f3f5946341a0bdfd9ad
Alissa Reynolds <alissa6710@gmail.com>
2023-09-24 16:39:48 -0500

    first commit of extra credit

---
+++ extraCredit.js
@@ -0,0 +1,16 @@
+/*
+   Three tech companies that I would be interested in joining include Cognizant, Accenture, and Tata Consultancy Services. The main company I would like to join is Cognizant, as they are an American multinational information technology services and consulting company. This company has relationships with 29 of out the 30 global pharma companies, 9 out of the 10 ten European banks, 7 of the top 10 interest companies, and 23 out of the top 25 healthcare plans. Cognizant offers various services based off of different technology stacks, although one of its new technology stacks that they are currently promoting is called DIGITALL. They believe that this new stack has the potential to help advance the IT industry. The DIGITALL stack includes machine learning, quantum computing, digital humans, and robotic process automation. Overall, Cognizant uses a total of 99 technology products and services such as Angular, Spring Boot, and full stack including JavaScript and Java. It says currently there is 1651 jobs avaliable in technology and engineering through Cognizant. This companys says they value continuous learning and adaptability to stay updated with the changing technology landscape. Cognizant looks for candidates who possess a combination of problem-solving skills, collaboration abilites, and someone with great communication skills. 
+
+   Another compnay that I would be interested in is Accenture. Accenture is an Irish-American professional services company that specializes in technology services and consulting. The main technologies used by Accenture are HubSpot CRM, Prezi, and Conga Contracts. Accenture also uses C, C++, Java, and Python as their preferred languages for coding sofware. They usually test their candidates on problem-solving, technical skills, and logical thinking. This company also uses innovative technologies such as AI and robotics to further their competitive advantage. Accenture advertises a wide range of technology capabilities such as infrastructure engineering, platform services, security, and software engineering. They are also a managed service provider for Azure, Google Cloud, and AWS. 
+
+   The third company I have been conducting research on is Tata Consultancy Services. TCS is an Indian multinational information technology services and consulting company. It is the second largest Indian Company by market capitalization, and is one of the most valuable IT service brands. They say that their workforce includes 615k employees with 154 nationalities represented across the globe, and an 84 percent increase in women executive roles. TCS uses 10 technology products and services including HTML5, jQuery, and Google Analytics. They also use Java, C, C++, Python, and Perl as their main coding languages. Although C++ is the preferred language for competitive coding at their company. This company also operates in 150 locations across 46 countries, but with its headquarters in Mumbai India. 
+
+
+
+
+
+
+
+
+
+*/ 
